2854df7cf634d69fa1576240fb81e9249f1f7c9e
Saifullah Ansari <saifa5098@gmail.com>
2024-05-15 12:16:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 const url = require('url');
 // Middleware
+
 app.use(cors());
 app.use(bodyParser.json());
 function extractWebsiteName(domain) {
